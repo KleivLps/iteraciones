d30ad575a3c6d567b86e3c6ff687b55188517525
kleiver <kleiver1604@gmail.com>
2024-05-16 20:23:13 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let suma = 0;
+const hastaDondeSumar = prompt("Hasta que numero quiere sumar")
+
+for (let i = 1; i <= hastaDondeSumar; i ++){
+    suma = suma + i
+}
+
+alert ("El resutado de la suma es : "+suma);
